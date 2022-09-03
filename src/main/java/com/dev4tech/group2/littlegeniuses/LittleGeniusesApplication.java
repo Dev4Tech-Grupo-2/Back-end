b6a5790b71f7f901086e797432ea4c820e378679
Renato Viana <renatoviana30@gmail.com>
2022-09-03 12:00:55 -0300

    Initial project with spring boot and java 11

---
+++ src/main/java/com/dev4tech/group2/littlegeniuses/LittleGeniusesApplication.java
@@ -0,0 +1,13 @@
+package com.dev4tech.group2.littlegeniuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleGeniusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleGeniusesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev4tech/group2/littlegeniuses/LittleGeniusesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4tech.group2.littlegeniuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleGeniusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
